c06a5a9ace9f881e53da6d66c24325ffb1dece3e
stevegustason <steve.gustason@gmail.com>
2023-03-21 20:37:53 -0500

    Initial Commit

---
+++ Flag Guessing Game/Flag_Guessing_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flag_Guessing_GameApp.swift
+//  Flag Guessing Game
+//
+//  Created by Steven Gustason on 3/21/23.
+//
+
+import SwiftUI
+
+@main
+struct Flag_Guessing_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
